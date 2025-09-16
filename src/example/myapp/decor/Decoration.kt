9eb0d9f713758858f1c22f7140a848fde79467aa
Jorge <jdurancruz@danielcastelao.org>
2025-09-16 17:53:50 +0200

    Pruebas Enum

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -19,6 +19,7 @@ fun makeDecorations() {
 
 fun main(){
     makeDecorations2()
+    pruebaEnum()
 }
 
 // Here is a data class with 3 properties.
@@ -38,4 +39,21 @@ fun makeDecorations2() {
     //Puedes omitir variables con un barra baja "_"
     val (rock1, _, diver1) = d5
 
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun pruebaEnum() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+
+    println(Color.GREEN.rgb)
+    println(Color.GREEN.ordinal)
 }
