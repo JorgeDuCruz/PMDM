0187336825c40f3ebc7e5c3f68c9691e36b17c5e
Jorge <jdurancruz@danielcastelao.org>
2025-09-16 17:46:26 +0200

    Desestrucutracion hecha

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -18,5 +18,24 @@ fun makeDecorations() {
 }
 
 fun main(){
-    makeDecorations()
+    makeDecorations2()
+}
+
+// Here is a data class with 3 properties.
+data class Decoration2(val rocks: String, val wood: String, val diver: String){
+}
+
+fun makeDecorations2() {
+    val d5 = Decoration2("crystal", "forest", "car")
+    println(d5)
+
+// Asigna las variables segun el orden de aparicion a menos que coincidadan los nombres. Si coninciden, cada variable toma el valor de su homologo en la clase datos
+    val (rock, diver,wood) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+
+    //Puedes omitir variables con un barra baja "_"
+    val (rock1, _, diver1) = d5
+
 }
