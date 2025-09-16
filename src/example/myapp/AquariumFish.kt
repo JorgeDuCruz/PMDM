c64a194331dca419db33d2ad9c6e2c705b510568
dam <jdurancruz@danielcastelao.org>
2025-09-16 12:53:05 +0200

    Interface y abstract class

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -1,9 +1,5 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
-}
-
 class Shark: AquariumFish(), FishAction {
     override val color = "gray"
     override fun eat() {
@@ -20,4 +16,18 @@ class Plecostomus: AquariumFish(), FishAction {
 
 interface FishAction{
     fun eat()
+}
+abstract class AquariumFish: FishAction {
+    abstract val color: String
+    override fun eat() = println("yum")
+}
+
+interface AquariumAction {
+    fun eat()
+    fun jump()
+    fun clean()
+    fun catchFish()
+    fun swim()  {
+        println("swim")
+    }
 }
