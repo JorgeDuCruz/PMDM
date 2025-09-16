24f04c815034bf432ea5c957fa942ff1d18ff6e7
dam <jdurancruz@danielcastelao.org>
2025-09-16 11:39:19 +0200

    probar towertank

--- src/example/myapp/Main.kt
+++ src/example/myapp/Main.kt
@@ -21,6 +21,9 @@ fun buildAquarium() {
     val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
 
+    val myTowerTank = TowerTank(diameter = 25, height = 40)
+    myTowerTank.printSize()
+
 
 }
 
