d64ab953c08b5cc9755ab34a5aa1de6a983d32b2
dam <jdurancruz@danielcastelao.org>
2025-09-16 11:18:01 +0200

    Set volume

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,9 +1,11 @@
 package example.myapp
 
 class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
-    val volume: Int
+    var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 l
-
+        set(value){
+            height = (value * 1000) / (width * length)
+        }
     init {
         println("aquarium initializing")
     }
